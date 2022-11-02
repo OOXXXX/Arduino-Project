8cc8f04f9bb21a6f3b3a6af9f976f5c7d2368129
OOXXXX <zhupx2003@126.com>
2022-11-02 01:12:27 +0000

    Add a sound reactor

--- Ultrasound_Test1/base.ino
+++ Ultrasound_Test1/base.ino
@@ -1,23 +1,33 @@
 const int trigPin = 9;
 const int echoPin = 10;
 const int ledPin = 7;
+const int buzzerPin = 6;
 
 int duration;
 float distance;
+long frequency;
 
 void setup() {
 pinMode(trigPin, OUTPUT); 
 pinMode(echoPin, INPUT);
 pinMode (ledPin, OUTPUT); 
+pinMode (buzzerPin, OUTPUT);
 Serial.begin(9600); 
 digitalWrite (ledPin, LOW);
 }
 
 void LedFlash16() {
     digitalWrite (ledPin, HIGH);
-    delay (600);
+    delay (400);
     digitalWrite (ledPin, LOW);
-    delay (600);
+    delay (400);
+}
+
+void Buzz16() {
+    tone(buzzerPin, frequency);
+    delay(400); 
+    noTone(buzzerPin);
+    delay(400); 
 }
 
 void LedFlash11() {
@@ -27,11 +37,25 @@ void LedFlash11() {
     delay (200);
 }
 
+void Buzz11() {
+    tone(buzzerPin, frequency);
+    delay(200); 
+    noTone(buzzerPin);
+    delay(200); 
+}
+
 void LedFlash6() {
     digitalWrite (ledPin, HIGH);
-    delay (100);
+    delay (80);
     digitalWrite (ledPin, LOW);
-    delay (100);
+    delay (80);
+}
+
+void Buzz6() {
+    tone(buzzerPin, frequency);
+    delay(80); 
+    noTone(buzzerPin);
+    delay(80); 
 }
 
 void loop() {
@@ -40,20 +64,24 @@ delayMicroseconds(2);
 digitalWrite(trigPin, HIGH);
 delayMicroseconds(10);
 digitalWrite(trigPin, LOW);
+frequency = 1000;
 duration = pulseIn(echoPin, HIGH);
 distance = duration * 0.034 / 2;
 
     if (distance <= 16 && distance >= 11) {
         Serial.println ("Someone Is Infront Of the Sensor");
         LedFlash16();
+        Buzz16();
     }
     else if (distance <= 11 && distance >= 6) {
         Serial.println ("Someone Is Infront Of the Sensor");
         LedFlash11();
+        Buzz11(); 
     }
     else if (distance < 6) {
         Serial.println ("Someone Is Infront Of the Sensor");
         LedFlash6();
+        Buzz6();
     }
     else {
         Serial.println ("Nobody Is Infront Of the Sensor");
