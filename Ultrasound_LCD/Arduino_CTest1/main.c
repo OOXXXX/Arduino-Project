fbf1fa5f3914317e102c4db87b39d0571598d4f7
OOXXXX <zhupx2003@126.com>
2022-11-05 02:45:06 +0000

    Added more if component

--- Ultrasound_LCD/Arduino_CTest1/main.c
+++ Ultrasound_LCD/Arduino_CTest1/main.c
@@ -4,7 +4,15 @@
 #include <stdio.h>
 
 void LedFlash16() {
-    printf("There's something behind the sensor!\nLED flashes at 0.8Hz\nBuzzer makes sound at 400Hz");
+    printf("There's something behind the sensor!\nLED flashes at 1.25 Hz\nBuzzer makes sound at 300Hz");
+}
+
+void LedFlash11() {
+    printf("There's something behind the sensor!\nLED flashes at 2.5 Hz\nBuzzer makes sound at 700Hz");
+}
+
+void LedFlash6() {
+    printf("There's something behind the sensor!\nLED flashes at 5 Hz\nBuzzer makes sound at 1100Hz");
 }
 
 void LedNotFlash() {
@@ -13,13 +21,20 @@ void LedNotFlash() {
 
 int main() {
     int Distance;
-    printf("Please enter the distance of object to sensor: ");
+    printf("Please enter the distance of the object to sensor: ");
     scanf("%d", &Distance);
-    if (Distance < 16) {
+    if (Distance <= 16 && Distance >= 11) {
         LedFlash16();
+    }
+    else if (Distance < 11 && Distance >= 6) {
+        LedFlash11();
+    }
+    else if (Distance < 6) {
+        LedFlash6();
     }
     else {
         LedNotFlash();
+        //printf("There's nothing behind the sensor!\nLED not flash\nBuzzer makes no sound\nThe LCD screen will display\nDistance:\n%d",Distance);
     }
 
     return 0;
