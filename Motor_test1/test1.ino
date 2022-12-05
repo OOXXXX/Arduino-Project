45bce8bb87502413e5a809d72aa9afee532f4875
OOXXXX <zhupx2003@126.com>
2022-12-05 15:14:45 +0000

    optimized speed sensor

--- Motor_test1/test1.ino
+++ Motor_test1/test1.ino
@@ -1,40 +1,33 @@
-int sensor = 7;
 const int motorPin = 6; 
-unsigned long start_time = 0;
-unsigned long end_time = 0;
-int steps=0;
-float steps_old=0;
-float temp=0;
-float rps=0;
-float rpm=0;
+const int echoPin = 7;
+float rps;
+float rpm;
+int time1;
+int time2;
 
 void setup()
 {
+  pinMode(echoPin, INPUT);
   Serial.begin(115200);
-  pinMode(sensor,INPUT_PULLUP);
-  pinMode(motorPin, OUTPUT); 
 }
 void loop()
 {
-  for (int a = 0; a <= 255; a++) 
+  time1 = pulseIn(echoPin, HIGH);
+  time2 = pulseIn(echoPin, LOW);
+  rps = 1000000/(2*(time1+time2));
+  rpm = rps*60;
+
+  if (rps == -1)
+  {
+    Serial.println("No signal");
+  }
+  else
+  {
+    Serial.println(rpm);
+  }
+  
+  for (int a = 0; a <= 200; a++) 
   {
     analogWrite(motorPin, a); 
   }
-
-  start_time=millis();
-  end_time=start_time+1000;
-  while(millis()<end_time)
- {
-   if(digitalRead(sensor))
-   {
-    steps=steps+1; 
-    while(digitalRead(sensor));
-   }
- }
-  temp=steps-steps_old;
-  steps_old=steps;
-  rps = (temp/20);
-  rpm = rps*60;
-  //Serial.println(rps);
-  Serial.println(rpm);
 }
